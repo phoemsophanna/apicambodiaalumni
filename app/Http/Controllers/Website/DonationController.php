3fcb5956d2e1ef1a11c11c98dc12e0b7d8ab52ab
phoemsophanna <phannaphoem@gmail.com>
2026-02-02 08:53:23 +0700

    Update some code

--- app/Http/Controllers/Website/DonationController.php
+++ app/Http/Controllers/Website/DonationController.php
@@ -147,8 +147,8 @@ class DonationController extends Controller
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS => array(
                     'chat_id' => '-5148085853',
-                    'text' => '<b>Donation:</b>'."\n".'
-                    <b><u>Project:</u></b> ' . ($campaign ? $campaign['campaignTile'] : '') . '
+                    'text' => '<b>Donation:</b>
+<b><u>Project:</u></b> ' . ($campaign ? $campaign['campaignTile'] : '') . '
 <b><u>Donor Info:</u></b>
 <code>' . ($user ? $user['name'] : "Anonymous") . '</code>
 <code>' . ($user ? $user['phoneNumber'] : "") . '</code>
