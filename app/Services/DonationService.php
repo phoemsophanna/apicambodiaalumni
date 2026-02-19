9f562d72bb2382d3e3ed9b405a9ec5b84971ce36
phoemsophanna <phannaphoem@gmail.com>
2026-02-19 09:50:53 +0700

    Update some code

--- app/Services/DonationService.php
+++ app/Services/DonationService.php
@@ -19,7 +19,7 @@ class DonationService
                 $donation = Donation::where('id', $id)->where("paymentStatus", "!=", "APPROVED")->first();
                 if ($donation) {
                     $value = self::getTransactionDetail($donation->requestTime, $donation->transactionId);
-                    $payment_type = self::checkPaymentType($value->payment_type);
+                    $payment_type = self::checkPaymentType($value['payment_type']);
                     $donation->paymentStatus = "APPROVED";
                     $donation->paymentMethod = $payment_type;
                     $donation->save();
