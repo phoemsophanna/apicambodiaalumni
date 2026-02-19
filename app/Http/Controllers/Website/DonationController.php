04287c8e7b5d986df66d5eea4be6ff0fad797a17
phoemsophanna <phannaphoem@gmail.com>
2026-02-19 09:04:59 +0700

    Update some code

--- app/Http/Controllers/Website/DonationController.php
+++ app/Http/Controllers/Website/DonationController.php
@@ -120,7 +120,7 @@ class DonationController extends Controller
         ], 200);
     }
 
-    public static function checkOrder($transactionId)
+    public function checkOrder($transactionId)
     {
         try {
             DB::transaction(function () use ($transactionId) {
--- app/Services/DonationService.php
+++ app/Services/DonationService.php
@@ -3,16 +3,97 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Log;
-use App\Http\Controllers\Website\DonationController;
 use App\Models\Donation;
+use App\Models\User;
+use App\Models\Campaign;
 
 class DonationService
 {
-    public static function approved($id)
+    public function approved($id)
     {
-        $donation = Donation::where("paymentStatus", "DRAFT")->where("id", $id)->first();
-        if($donation) {
-            DonationController::checkOrder($donation->transactionId);
+        try {
+            DB::transaction(function () use ($id) {
+                $donation = Donation::where('id', $id)->where("paymentStatus", "!=", "APPROVED")->first();
+                if ($donation) {
+                    $donation->paymentStatus = "APPROVED";
+                    $donation->save();
+
+                    $campaign = Campaign::where("id", $donation->campaignId)->first();
+                    if($campaign){
+                        Campaign::where("id", $donation->campaignId)->update([
+                            "totalTip" => $campaign->totalTip + $donation->tip,
+                            "totalRaised" => $campaign->totalRaised + $donation->amount,
+                            "balance" => $campaign->balance + $donation->amount,
+                            "totalDonation" => $campaign->totalDonation + 1
+                        ]);
+                    }
+
+                    $user = User::where("id", $donation->donorId)->first();
+                    if($user) {
+                        User::where("id", $donation->donorId)->update([
+                            "totalDonation" => $user->totalDonation + ($donation->tip + $donation->amount)
+                        ]);
+                        Feed::create([
+                            "creatorId" => $user->id,
+                            "feedType" => "DONATION",
+                            "campaignId" => $donation->campaignId ? $donation->campaignId : 0,
+                            "donationId" => $donation->id,
+                            "publishedAt" => Carbon::now()
+                        ]);
+                    }
+
+                    $notification = $this->sentNotification($donation, $user, $campaign);
+                    
+                    if(!$notification->status){
+                        return response()->json([
+                            "message" => "Sent Notification Fail",
+                            "error" => $notification->message,
+                            "status" => "fail"
+                        ], 200);
+                    }
+                    
+                    Log::alert("Update Donation{$transactionId} Success.");
+                }
+            });
+        } catch (Exception $th) {
+            Log::alert("Update Order Fail: " + $th->getMessage());
         }
+    }
+
+    private static function sentNotification($item, $user = null, $campaign = null) {
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.telegram.org/bot8590989809:AAEuh7sDgw5alUoQv76QzdOfYbJ2BCqGITo/sendMessage",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'chat_id' => '-1003584798129',
+                    'text' => '<b>Donation:</b>
+' . ($campaign ? '<b><u>Project:</u></b> ' . $campaign['campaignTile'] : '') . '
+<b><u>Donor Info:</u></b>
+<code>' . ($user ? $user['name'] : "Anonymous") . '</code>
+<code>' . ($user ? $user['phoneNumber'] : "") . '</code>
+<b>Payment Method:</b> <u>' . $item['paymentMethod'] . '</u> ' . '
+<b>Amount:</b> <u>' . number_format($item['total'], 2, '.') . '</u> ' . '
+<b><u>Note:</u></b> ' . "\n" . $item['note'],
+                    'parse_mode' => 'HTML'
+                ),
+            ));
+
+            curl_exec($curl);
+
+            curl_close($curl);
+        } catch (Exception $th) {
+            // Log::error($th->getMessage(), $th);
+            return (object)['message' => $th->getMessage(), "status" => false];
+        }
+        return (object)["status" => true];
     }
 }
