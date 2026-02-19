a986824af107fe31162abe049765be06bf61b381
phoemsophanna <phannaphoem@gmail.com>
2026-02-19 09:19:39 +0700

    Update some code

--- app/Services/DonationService.php
+++ app/Services/DonationService.php
@@ -64,39 +64,39 @@ class DonationService
     }
 
     private static function sentNotification($item, $user = null, $campaign = null) {
-//         try {
-//             $curl = curl_init();
+        try {
+            $curl = curl_init();
 
-//             curl_setopt_array($curl, array(
-//                 CURLOPT_URL => "https://api.telegram.org/bot8590989809:AAEuh7sDgw5alUoQv76QzdOfYbJ2BCqGITo/sendMessage",
-//                 CURLOPT_RETURNTRANSFER => true,
-//                 CURLOPT_ENCODING => '',
-//                 CURLOPT_MAXREDIRS => 10,
-//                 CURLOPT_TIMEOUT => 0,
-//                 CURLOPT_FOLLOWLOCATION => true,
-//                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//                 CURLOPT_CUSTOMREQUEST => 'POST',
-//                 CURLOPT_POSTFIELDS => array(
-//                     'chat_id' => '-1003584798129',
-//                     'text' => '<b>Donation:</b>
-// ' . ($campaign ? '<b><u>Project:</u></b> ' . $campaign['campaignTile'] : '') . '
-// <b><u>Donor Info:</u></b>
-// <code>' . ($user ? $user['name'] : "Anonymous") . '</code>
-// <code>' . ($user ? $user['phoneNumber'] : "") . '</code>
-// <b>Payment Method:</b> <u>' . $item['paymentMethod'] . '</u> ' . '
-// <b>Amount:</b> <u>' . number_format($item['total'], 2, '.') . '</u> ' . '
-// <b><u>Note:</u></b> ' . "\n" . $item['note'],
-//                     'parse_mode' => 'HTML'
-//                 ),
-//             ));
+            curl_setopt_array($curl, array(
+                // CURLOPT_URL => "https://api.telegram.org/bot8590989809:AAEuh7sDgw5alUoQv76QzdOfYbJ2BCqGITo/sendMessage",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'chat_id' => '-1003584798129',
+                    'text' => '<b>Donation:</b>
+' . ($campaign ? '<b><u>Project:</u></b> ' . $campaign['campaignTile'] : '') . '
+<b><u>Donor Info:</u></b>
+<code>' . ($user ? $user['name'] : "Anonymous") . '</code>
+<code>' . ($user ? $user['phoneNumber'] : "") . '</code>
+<b>Payment Method:</b> <u>' . $item['paymentMethod'] . '</u> ' . '
+<b>Amount:</b> <u>' . number_format($item['total'], 2, '.') . '</u> ' . '
+<b><u>Note:</u></b> ' . "\n" . $item['note'],
+                    'parse_mode' => 'HTML'
+                ),
+            ));
 
-//             curl_exec($curl);
+            curl_exec($curl);
 
-//             curl_close($curl);
-//         } catch (Exception $th) {
-//             // Log::error($th->getMessage(), $th);
-//             return (object)['message' => $th->getMessage(), "status" => false];
-//         }
-//         return (object)["status" => true];
+            curl_close($curl);
+        } catch (Exception $th) {
+            // Log::error($th->getMessage(), $th);
+            return (object)['message' => $th->getMessage(), "status" => false];
+        }
+        return (object)["status" => true];
     }
 }
